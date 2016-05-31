1ac56c73a435e06678dc25214ba78630590e5191
Ilay Volkov <volchonek@gmail.com>
2016-06-01 01:14:22 +0300

    Calculator

---
+++ Сalculator/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сalculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Допилить факториал и числа с плавающей запятой + телефонный справочник
+
+			//ввод данных
+			Console.WriteLine("Программа калькулятор");
+			Console.WriteLine("Программа умеет считать x*y, x/y, x+y, x-y, x!, x^y как по отдельности, так и в сочетании друг с другом ");
+			Console.WriteLine("_______________________________________________________________________________________________________");
+			Console.WriteLine("Введите пример, который необходимо посчитать");
+			var s = Console.ReadLine().ToString();
+			BigInteger r = Parse(s);
+			Console.WriteLine("");
+			Console.WriteLine("Результат");
+			Console.WriteLine("{0} = {1}", s, r);
+			Console.ReadKey();
+		}
+		//разбор строки
+		static BigInteger Parse(string s)
+		{
+			//задаем начальный индекс
+			int index = 0;
+			//получаем число с номером 
+			BigInteger v = GetNumber(s, ref index);
+			while (index < s.Length)
+			{
+				switch (s[index])
+				{
+					case '+'://операция сложения
+						index++;
+						v += MulDiv(s, ref index);
+						break;
+					case '-'://операция вычитания
+						index++;
+						v -= MulDiv(s, ref index);
+						break;
+					case '*'://операция умножения
+						index++;
+						v *= StepenChislaFunction(s, ref index);
+						break;
+					case '/'://операция деления
+						index++;
+						v /= StepenChislaFunction(s, ref index);
+						break;
+					case '!'://операция вычисление факториала
+						index++;
+						v = Factorial(v);
+						break;
+					case '^'://операция вычисление "v" в степени "w"                            
+						index++;
+						BigInteger w = GetNumber(s, ref index);
+						v = StepenChisla(v, w);
+						break;
+					default:
+						Console.WriteLine("Error");
+						break;
+				}
+			}
+			return v;
+		}
+		static BigInteger GetNumber(string s, ref int index)
+		{
+			var tmp = "";
+			//отсекаем строки
+			foreach (char c in s.Substring(index))
+			{
+				//проверяем посимвольно каждый символ строки
+				if (!char.IsDigit(c))
+				{
+					break;
+				}
+				index++;
+				tmp += c;
+			}
+			return BigInteger.Parse(tmp);//формируем число из строки
+		}
+		static BigInteger MulDiv(string s, ref int index)
+		{
+			//получаем число с номером 
+			BigInteger v = GetNumber(s, ref index);
+			while (index < s.Length)
+			{
+				if (s[index] == '*')
+				{
+					//операция умножения
+					index++;
+					v *= StepenChislaFunction(s, ref index);
+				}
+				else if (s[index] == '/')
+				{
+					//операция деления
+					index++;
+					v /= StepenChislaFunction(s, ref index);
+				}
+				else if (s[index] == '^')
+				{
+					//операция вычисление "v" в степени "w"  
+					index++;
+					BigInteger w = GetNumber(s, ref index);
+					v = StepenChisla(v, w);
+				}
+				else if (s[index] == '!')
+				{
+					//операция вычисление факториала 
+					index++;
+					v = Factorial(v);
+				}
+				else
+				{
+					break;
+				}
+
+			}
+			return v;
+		}
+		//функция степени числа
+		static BigInteger StepenChislaFunction(string s, ref int index)
+		{
+			//получаем число с номером 
+			BigInteger v = FactorialFunction(s, ref index);
+			while (index < s.Length)
+			{
+				if (s[index] == '^')
+				{
+					index++;
+					//получаем число с номером 
+					BigInteger w = FactorialFunction(s, ref index);
+					//операция вычисление "v" в степени "w"                  
+					v = StepenChisla(v, w);
+				}
+				else
+				{
+					break;
+				}
+			}
+			return v;
+		}
+		//вычисление степени числа
+		static BigInteger StepenChisla(BigInteger v, BigInteger w)
+		{
+			if (w == 0)
+			{
+				v = 1;
+			}
+			else if (w == 1)
+			{
+				return v;
+			}
+			else
+			{
+				var n = v;
+				for (int i = 2; i <= w; i++)
+				{
+					v = v * n;
+				};
+			};
+			return v;
+		}
+		//функция факториала
+		static BigInteger FactorialFunction(string s, ref int index)
+		{
+			//получаем число с номером 
+			BigInteger v = GetNumber(s, ref index);
+			while (index < s.Length)
+			{
+				if (s[index] == '!')
+				{
+					//операция вычисление факториала 
+					index++;
+					v = Factorial(v);
+				}
+				else
+				{
+					break;
+				}
+
+			}
+			return v;
+		}
+		//вычисление факториала
+		static BigInteger Factorial(BigInteger v)
+		{
+			int f = 1;
+			for (int i = 2; i < (v + 1); ++i)
+			{
+				f = f * i;
+			}
+			v = f;
+			return v;
+		}
+	}
+}
---
+++ Сalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Сalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Сalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f09fa16f-6bc2-4595-8ca4-4388c35b7ea4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
